0bc49bc0ba8b32275a3d15d6ad9ab4b3ec877e50
kqueej <alexandrd542@gmail.com>
2026-09-25 11:08:31 +0300

    Lab06: method overloading

--- Program.cs
+++ Program.cs
@@ -31,28 +31,47 @@
 //     Console.WriteLine($"{itemName}: {price} руб.{discountLabel}");
 // }
 
-Console.WriteLine();
-Console.WriteLine($"5 + 10 = {Add(5, 10)}");
-Console.WriteLine($"Площадь 2x2 больше площади 1x5: {CalculateRectangleArea(2, 2) > CalculateRectangleArea(1, 5)}");
+// Console.WriteLine();
+// Console.WriteLine($"5 + 10 = {Add(5, 10)}");
+// Console.WriteLine($"Площадь 2x2 больше площади 1x5: {CalculateRectangleArea(2, 2) > CalculateRectangleArea(1, 5)}");
 
+// int sum = Add(15, 27);
+// Console.WriteLine($"Сумма: {sum}");
 
-int sum = Add(15, 27);
-Console.WriteLine($"Сумма: {sum}");
+// double area = CalculateRectangleArea(3.5, 4.2);
+// Console.WriteLine($"Площадь прямоугольника: {area}");
 
-double area = CalculateRectangleArea(3.5, 4.2);
-Console.WriteLine($"Площадь прямоугольника: {area}");
+// bool isAdult = IsAdult(20);
+// Console.WriteLine($"Совершеннолетний: {isAdult}");
 
-bool isAdult = IsAdult(20);
-Console.WriteLine($"Совершеннолетний: {isAdult}");
+// int Add(int a, int b) {
+//     return a + b;
+// }
 
-int Add(int a, int b) {
-    return a + b;
-}
+// double CalculateRectangleArea(double width, double height) {
+//     return width * height;
+// }
 
-double CalculateRectangleArea(double width, double height) {
-    return width * height;
-}
+// bool IsAdult(int age) {
+//     return age >= 18;
+// }
 
-bool IsAdult(int age) {
-    return age >= 18;
+class Program {
+    static void Main() {
+        Console.WriteLine(Add(2, 3));  // вызовет первый вариант: int Add(int, int) → 5
+        Console.WriteLine(Add(2.5, 3.5)); // вызовет второй вариант: double Add(double, double) → 6.0
+        Console.WriteLine(Add(2, 3, 4)); // вызовет третий вариант: int Add(int, int, int) → 9
+    }
+
+    static int Add(int a, int b) {
+        return a + b;
+    }
+
+    static double Add(double a, double b) {
+        return a + b;
+    }
+
+    static int Add(int a, int b, int c) {
+        return a + b + c;
+    }
 }
