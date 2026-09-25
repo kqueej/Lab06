4a8415d907d105251e7a677a9bf8dbdae29a1580
kqueej <alexandrd542@gmail.com>
2026-09-25 11:04:07 +0300

    Lab06: methods with parameters

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿PrintHeader();
 PrintHeader();
+
 Console.WriteLine("Основная часть программы");
 PrintFooter();
+
+PrintStudentCard("Иванов Иван", "ИСП-221", 2);
+PrintStudentCard("Смирнова Анна", "ИСП-222", 2);
+
+PrintPurchase("Ноутбук", 65000, true);
+PrintPurchase("Мышь", 1200, false);
 
 void PrintHeader() {
     Console.WriteLine("^_^");
@@ -13,4 +20,13 @@ void PrintFooter() {
     Console.WriteLine(">_<");
     Console.WriteLine("   Конец программы");
     Console.WriteLine("X_X");
+}
+
+void PrintStudentCard(string name, string group, int course) {
+    Console.WriteLine($"Студент: {name}, группа {group}, курс {course}");
+}
+
+void PrintPurchase(string itemName, double price, bool hasDiscount) {
+    string discountLabel = hasDiscount ? " (со скидкой)" : "";
+    Console.WriteLine($"{itemName}: {price} руб.{discountLabel}");
 }
