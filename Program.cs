99f41cbb54d7ed3e7d285df1b2d6e22439d276c4
kqueej <alexandrd542@gmail.com>
2026-09-25 11:05:46 +0300

    Lab06: methods with return values

--- Program.cs
+++ Program.cs
@@ -1,32 +1,58 @@
-﻿PrintHeader();
-PrintHeader();
+﻿// PrintHeader();
+// PrintHeader();
 
-Console.WriteLine("Основная часть программы");
-PrintFooter();
+// Console.WriteLine("Основная часть программы");
+// PrintFooter();
 
-PrintStudentCard("Иванов Иван", "ИСП-221", 2);
-PrintStudentCard("Смирнова Анна", "ИСП-222", 2);
+// PrintStudentCard("Иванов Иван", "ИСП-221", 2);
+// PrintStudentCard("Смирнова Анна", "ИСП-222", 2);
 
-PrintPurchase("Ноутбук", 65000, true);
-PrintPurchase("Мышь", 1200, false);
+// PrintPurchase("Ноутбук", 65000, true);
+// PrintPurchase("Мышь", 1200, false);
 
-void PrintHeader() {
-    Console.WriteLine("^_^");
-    Console.WriteLine("Лабораторная работа №6");
-    Console.WriteLine("0_0");
+// void PrintHeader() {
+//     Console.WriteLine("^_^");
+//     Console.WriteLine("Лабораторная работа №6");
+//     Console.WriteLine("0_0");
+// }
+
+// void PrintFooter() {
+//     Console.WriteLine(">_<");
+//     Console.WriteLine("   Конец программы");
+//     Console.WriteLine("X_X");
+// }
+
+// void PrintStudentCard(string name, string group, int course) {
+//     Console.WriteLine($"Студент: {name}, группа {group}, курс {course}");
+// }
+
+// void PrintPurchase(string itemName, double price, bool hasDiscount) {
+//     string discountLabel = hasDiscount ? " (со скидкой)" : "";
+//     Console.WriteLine($"{itemName}: {price} руб.{discountLabel}");
+// }
+
+Console.WriteLine();
+Console.WriteLine($"5 + 10 = {Add(5, 10)}");
+Console.WriteLine($"Площадь 2x2 больше площади 1x5: {CalculateRectangleArea(2, 2) > CalculateRectangleArea(1, 5)}");
+
+
+int sum = Add(15, 27);
+Console.WriteLine($"Сумма: {sum}");
+
+double area = CalculateRectangleArea(3.5, 4.2);
+Console.WriteLine($"Площадь прямоугольника: {area}");
+
+bool isAdult = IsAdult(20);
+Console.WriteLine($"Совершеннолетний: {isAdult}");
+
+int Add(int a, int b) {
+    return a + b;
 }
 
-void PrintFooter() {
-    Console.WriteLine(">_<");
-    Console.WriteLine("   Конец программы");
-    Console.WriteLine("X_X");
+double CalculateRectangleArea(double width, double height) {
+    return width * height;
 }
 
-void PrintStudentCard(string name, string group, int course) {
-    Console.WriteLine($"Студент: {name}, группа {group}, курс {course}");
-}
-
-void PrintPurchase(string itemName, double price, bool hasDiscount) {
-    string discountLabel = hasDiscount ? " (со скидкой)" : "";
-    Console.WriteLine($"{itemName}: {price} руб.{discountLabel}");
+bool IsAdult(int age) {
+    return age >= 18;
 }
