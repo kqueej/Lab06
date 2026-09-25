3cfa8d49767e431374e1fea9102e6c49bfe96e4e
kqueej <alexandrd542@gmail.com>
2026-09-25 10:59:56 +0300

    Lab06: void methods without parameters

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿PrintHeader();
+PrintHeader();
+Console.WriteLine("Основная часть программы");
+PrintFooter();
+
+void PrintHeader() {
+    Console.WriteLine("^_^");
+    Console.WriteLine("Лабораторная работа №6");
+    Console.WriteLine("0_0");
+}
+
+void PrintFooter() {
+    Console.WriteLine(">_<");
+    Console.WriteLine("   Конец программы");
+    Console.WriteLine("X_X");
+}
